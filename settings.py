f2edf0d71c96f59ff1ecabba3b71dc1e9d72f31b
V-B-Bozhinskiy <bove203@outlook.com>
2025-10-30 17:06:58 +0300

    bullet

--- settings.py
+++ settings.py
@@ -9,4 +9,11 @@ class Settings():
         self.bg_color = (230, 230, 230)
 
         # Настройки корабля
-        self.ship_speed = 1.5 
+        self.ship_speed = 1.5
+
+        # Настройки снаряда
+        self.bullet_speed = 1
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
+        self.bullets_allowed = 3 
